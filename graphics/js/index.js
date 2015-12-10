bc0dd78e0ae08874ed265f151d4ba278a192b00d
pauly0594 <pramir@saic.edu>
2015-12-10 16:54:09 -0600

    new site up for business

---
+++ graphics/js/index.js
@@ -0,0 +1,28 @@
+// $('svg').click(function() {
+//     $('polygon').css({
+//         'opacity': '100'
+//     });
+// });
+
+/*$('polygon').hover(function() {
+    $('polygon').css({
+        'opacity': '0'
+    });
+});*/
+
+// setInterval(function() {
+//   $('polygon.caida')
+
+//     .velocity("callout.bounce",{
+//      stagger: 250 })
+//     .delay(750)
+//    .velocity("callout.tada",{
+//      stagger: 250 })
+//     .delay(750)
+//     .velocity({ stroke-opacity: 0 }, 0)
+ 
+// }, 100);
+
+
+
+
